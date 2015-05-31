0004ec3c425628613f05b08e8343401aeebad271
Dani Meana <danielmeanallorian@gmail.com>
2015-05-31 16:47:46 +0200

    Formated code

--- port_adapter.py
+++ port_adapter.py
@@ -2,13 +2,14 @@ import RPi.GPIO as GPIO
 
 __author__ = 'Dani'
 
+
 def setup(ports):
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     for row in ports:
         for port in row:
             GPIO.setup(port, GPIO.OUT)
-    
+
 
 def output(port, value):
     GPIO.output(port, value)
